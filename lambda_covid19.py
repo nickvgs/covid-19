68b8fb92677d7b25b9cc2b0bbdc9da2c1bd6bbcd
nickvgs <nick_.s@hotmail.com>
2022-12-03 15:56:30 -0300

    código de ETL finalizado

--- lambda_covid19.py
+++ lambda_covid19.py
@@ -3,13 +3,21 @@
 import pandas as pd 
 import numpy as np
 import glob
-
+import psycopg2 as pg
 # %%
-# definindo local de arquivis
+# definindo local de arquivos
 path = r'D:\Env\covid-19\data'
 csv_files = glob.glob(path + "/*.csv")
 
+# Definindo conexão com o banco de dados
 
+conn = pg.connect(database="data-cov19",
+                  host="localhost",
+                  user="postgres",
+                  password="admin",
+                  port="5432"
+                 )
+cur = conn.cursor()
 # %%
 # Lendo os arquivos CSV e Craindo o DF
 
@@ -23,4 +31,51 @@ df = pd.concat(dfs, ignore_index=True)
 
 # %%
 
+query = """INSERT INTO 
+           data_covid
+           (
+                regiao
+                estado
+                municipio
+                coduf
+                codmun
+                codregiaoSaude
+                nomeregiaoSaude
+                data
+                semanaepi
+                populacaotcu2019
+                casosacumulado
+                casosnovos
+                obitosacumulado
+                obitosnovos
+                recuperadosnovos
+                emacompanhamentonovos
+                interiormetropolitana
+            )
+            VALUES (
+                %s, %s, %s, %s, %s,
+                %s, %s, %s, %s, %s, 
+                %s, %s, %s, %s, %s,
+                %s, %s
+            ) 
+        """
+cur.execute(query, df)
+
 # %%
+regiao
+estado
+municipio
+coduf
+codmun
+codRegiaoSaude
+nomeRegiaoSaude
+data
+semanaEpi
+populacaoTCU2019
+casosAcumulado
+casosNovos
+obitosAcumulado
+obitosNovos
+Recuperadosnovos
+emAcompanhamentoNovos
+interioretropolitana
