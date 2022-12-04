4538a4f820522cdd42af15174e1d0f345a1112bd
nickvgs <nick_.s@hotmail.com>
2022-12-04 15:43:59 -0300

    script de criacao da tabela de banco de dados

---
+++ script.sql
@@ -0,0 +1,19 @@
+CREATE TABLE data_covid(
+				regiao varchar,
+				estado varchar,
+				municipio varchar,
+				coduf varchar,
+				codmun varchar,
+				codRegiaosaude varchar,
+				nomeRegiaoSaude varchar,
+				data varchar,
+				semanaepi varchar,
+				populacaotcu2019 varchar,
+				casosacumulado varchar,
+				casosnovos varchar,
+				obitosacumulado varchar,
+				obitosnovos varchar,
+				recuperadosnovos varchar,
+				emacompanhamentonovos varchar,
+				interiormetropolitana varchar
+);
